68ea4ff1a549072ad5d308b3460c29e67147c503
Ozan Eşki <34617144+ozaneski13@users.noreply.github.com>
2022-05-23 02:20:54 +0300

    Portal added. Portal light added. LaserBeamerLVL2 added.

---
+++ Assets/Scripts/Ligthing/PortalLightController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalLightController : MonoBehaviour
+{
+    [Header("Portal Light")]
+    [SerializeField] private Light _portalLight = null;
+
+    [Header("Intensity Thresholds")]
+    [SerializeField] private float _minIntensity = 10f;
+    [SerializeField] private float _maxIntensity = 20f;
+
+    [Header("Intensity Thresholds")]
+    [SerializeField] private float _flickerDuration = 0.1f;
+
+    private void Start()
+    {
+        StartCoroutine(LightFlicker());
+    }
+
+    private IEnumerator LightFlicker()
+    {
+        float increase = -1;
+
+        while(true)
+        {
+            if (_portalLight.intensity <= _minIntensity)
+                increase = 1;
+            else if (_portalLight.intensity >= _maxIntensity)
+                increase = -1;
+
+            _portalLight.intensity += increase;
+
+            yield return new WaitForSeconds(_flickerDuration);
+        }
+    }
+}
