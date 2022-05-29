0a7689d7efade9401cb565a7d82ec44d92b3a328
Ozan Eşki <34617144+ozaneski13@users.noreply.github.com>
2022-05-30 02:57:20 +0300

    Minor bug fix.

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class OptionsUI : MonoBehaviour
 {
+    private bool _isAlreadyPressed = false;
+
     public void ReturnToMenu()
     {
+        if (_isAlreadyPressed)
+            return;
+
+        _isAlreadyPressed = true;
+
         FadeUI.Instance.FadeTo(0);
     }
 }
